9c0ada6c7901d4d347028ef4533ac86e7971fb97
rgerrans <rgerrans@gmail.com>
2021-02-14 11:03:56 -0700

    Update nora-config.ts
    Adding structure

--- src/nodes/nora-config.ts
+++ src/nodes/nora-config.ts
@@ -10,7 +10,7 @@ module.exports = function (RED: any) {
             this.group = (config.group || '<default>').trim();
             this.valid = !!this.email?.length && !!this.password?.length;
             this.localExecution = config.localexecution ?? true;
-            this.structureHint = config.structureHint;
+            this.structureHint = config.structure;
 
             if (config.twofactor === 'pin' || config.twofactor === 'ack') {
                 const twoFactor: TwoFactor = {
