ed7e1b4af57a99035d66e2fee9f4da0d61600c2a
Philemon Schucker <philemon.schucker@gmx.de>
2021-09-26 00:49:00 +0200

    handle pendingAppSelectorCommand

--- src/nora/media-device.ts
+++ src/nora/media-device.ts
@@ -1,4 +1,5 @@
-import { ChannelDevice, TransportControlDevice, TransportControlIncomingCommand } from '@andrei-tatar/nora-firebase-common';
+import { AppSelectorDevice, ChannelDevice, TransportControlDevice,
+    TransportControlIncomingCommand } from '@andrei-tatar/nora-firebase-common';
 import { COMMAND_HANDLERS } from '@andrei-tatar/nora-firebase-common';
 import { child, DatabaseReference, DataSnapshot, onValue, remove } from 'firebase/database';
 import { merge, Observable, Subject } from 'rxjs';
@@ -9,11 +10,13 @@ import { FirebaseSync } from './sync';
 
 const TRANSPORT_CONTROL_COMMAND_PREFIX = 'action.devices.commands.media';
 const CHANNEL_COMMAND = /^action\.devices\.commands\.(?:return|relative|select)Channel$/;
+const APP_SELECTOR_COMMAND = /^action\.devices\.commands\.app(?:Search|Install)$/;
 
 export type ChannelChangeCommand = ChannelDevice['noraSpecific']['pendingChannelChangeCommand'];
-export type MediaCommand = TransportControlIncomingCommand | ChannelChangeCommand;
+export type AppSelectorCommand = AppSelectorDevice['noraSpecific']['pendingAppSelectorCommand'];
+export type MediaCommand = TransportControlIncomingCommand | ChannelChangeCommand | AppSelectorCommand;
 
-export class FirebaseMediaDevice<T extends (TransportControlDevice | ChannelDevice)> extends FirebaseDevice<T> {
+export class FirebaseMediaDevice<T extends (TransportControlDevice | ChannelDevice | AppSelectorDevice)> extends FirebaseDevice<T> {
     constructor(
         cloudId: string,
         sync: FirebaseSync,
@@ -25,11 +28,13 @@ export class FirebaseMediaDevice<T extends (TransportControlDevice | ChannelDevi
 
     private readonly transportControlCommand = child(this.noraSpecific, 'pendingTransportControlCommand');
     private readonly channelChangeCommand = child(this.noraSpecific, 'pendingChannelChangeCommand');
+    private readonly appSelectorCommand = child(this.noraSpecific, 'pendingAppSelectorCommand');
     private readonly localCommand$ = new Subject<MediaCommand>();
 
     readonly mediaCommand$ = merge(
         FirebaseMediaDevice.command<TransportControlIncomingCommand>(this.transportControlCommand),
         FirebaseMediaDevice.command<ChannelChangeCommand>(this.channelChangeCommand),
+        FirebaseMediaDevice.command<AppSelectorCommand>(this.appSelectorCommand),
         this.localCommand$,
     ).pipe(
         singleton(),
@@ -66,7 +71,15 @@ export class FirebaseMediaDevice<T extends (TransportControlDevice | ChannelDevi
                 this.localCommand$.next(result.updateNoraSpecific.pendingChannelChangeCommand);
                 return this.device.state;
             }
+        } else if (APP_SELECTOR_COMMAND.test(command)) {
+            const handler = COMMAND_HANDLERS.get(command);
+            const result = handler?.(this.device, params);
+            if (result?.updateNoraSpecific && 'pendingAppSelectorCommand' in result.updateNoraSpecific) {
+                this.localCommand$.next(result.updateNoraSpecific.pendingAppSelectorCommand);
+                return this.device.state;
+            }
         }
+
 
         return await super.executeCommand(command, params);
     }
