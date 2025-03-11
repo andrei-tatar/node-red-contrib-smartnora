e08de6f0c27483282cabf068d5a61f203af8c2e9
Andrei Tatar <tatarandrei@gmail.com>
2025-03-11 12:47:27 +0200

    ignore folders in eslint remove comments from built files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,25 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-  files: ["**/*.ts"],
-  extends: [eslint.configs.recommended, tseslint.configs.recommended],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-      },
-    ],
-    // ...
+export default tseslint.config(
+  {
+    ignores: ["**/build/**", "**/node_modules/**"],
   },
-});
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
+  }
+);
